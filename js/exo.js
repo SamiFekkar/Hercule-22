de4569aa08f81846af1aae7e8254d013ba9c9af4
SamiFekkar <mr.fekkar.sami@gmail.com>
2020-12-10 11:26:55 +0100

    correction bug final

--- js/exo.js
+++ js/exo.js
@@ -36,6 +36,7 @@ var hercule = {
   disponibilyElement: function () {
     if (base.getHour() >= 8 && base.getHour() <= 20) {
       document.getElementById("availability").innerHTML = "Disponible";
+      document.getElementById("availability").className = "on";
     } else {
       document.getElementById("availability").innerHTML = "Non disponible";
       document.getElementById("availability").className = "off";
@@ -55,19 +56,9 @@ var hercule = {
     document
       .getElementById("menu-toggler")
       .addEventListener("click", function () {
-        if (
-          document
-            .getElementById("header-banner")
-            .classList.contains("banner--open")
-        ) {
-          document
-            .getElementById("header-banner")
-            .classList.remove("banner--open");
-        } else {
-          document
-            .getElementById("header-banner")
-            .classList.add("banner--open");
-        }
+        document
+          .getElementById("header-banner")
+          .classList.toggle("banner--open");
       });
   },
   contactDisturb: function () {
@@ -100,7 +91,6 @@ var hercule = {
   },
   activities: function () {
     document.getElementById("activities").classList.remove("hidden");
-    tasks = [];
     for (let i = 0; i < base.activities.length; i++) {
       console.log(
         "base.activities[" + i + "].finished:",
@@ -115,10 +105,8 @@ var hercule = {
         activitiesElement.classList.remove("hidden");
         var liElement = document.createElement("li");
         activitiesElement.appendChild(liElement);
-        tasks.push(" " + base.activities[i].title);
         liElement.classList.add("tasks__item", "tasks__info");
-
-        liElement.innerHTML = tasks;
+        liElement.innerHTML = base.activities[i].title;
       }
     }
   },
